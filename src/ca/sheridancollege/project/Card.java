cb3e3558173bd8a70c3d73f35de72e591d93d823
kau17374 <kau17374@sheridancollege.ca>
2024-10-08 19:56:39 -0400

    Update Card.java

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -1,7 +1,7 @@
 /**
  * SYST 17796 Project Base code.
  * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
+ * Card.java modified by Anupreet Kaur (991752090) -TechBrains-!
  */
 package ca.sheridancollege.project;
 
@@ -9,17 +9,45 @@ package ca.sheridancollege.project;
  * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
  * game. Students wishing to add to the code should remember to add themselves as a modifier.
  *
- * @author dancye
+ * @author Anupreet Kaur Oct 2024
  */
-public abstract class Card {
-    //default modifier for child classes
+public class Card {
+    private Suit suit;
+    private Rank rank;
 
-    /**
-     * Students should implement this method for their specific children classes
-     *
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public int getValue() {
+        return rank.getValue();
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
     @Override
-    public abstract String toString();
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
 
+enum Suit {
+    HEARTS, DIAMONDS, CLUBS, SPADES
+}
+
+enum Rank {
+    TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(10), QUEEN(10), KING(10), ACE(11);
+
+    private final int value;
+
+    Rank(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
