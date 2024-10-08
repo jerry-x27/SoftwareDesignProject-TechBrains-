4bc4c68faa4c68979569a1c21fe92cd679805e8f
EshaarveerSingh <sineshaa@sheridancollege.ca>
2024-10-08 19:34:22 -0400

    Update Player.java

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -1,49 +1,50 @@
 /**
  * SYST 17796 Project Base code.
  * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
+ * Player.java modified by Eshaarveer Singh (991744745) -TechBrains-!
  */
 package ca.sheridancollege.project;
 
 /**
  * A class that models each Player in the game. Players have an identifier, which should be unique.
  *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
+ * @author Eshaarveer Singh Oct 2024
  */
-public abstract class Player {
+public class Player {
+    private String name;
+    private GroupOfCards hand;
 
-    private String name; //the unique name for this player
-
-    /**
-     * A constructor that allows you to set the player's unique ID
-     *
-     * @param name the unique ID to assign to this player.
-     */
     public Player(String name) {
         this.name = name;
+        this.hand = new GroupOfCards();
     }
 
-    /**
-     * @return the player name
-     */
+    public void addCard(Card card) {
+        hand.addCard(card);
+    }
+
+    public Card playCard(int index) {
+        return hand.removeCard(index);
+    }
+
+    public int getHandValue() {
+        return hand.getValue();
+    }
+
+    public boolean hasBusted() {
+        return getHandValue() > 21;
+    }
+
+    public GroupOfCards getHand() {
+        return hand;
+    }
+
     public String getName() {
         return name;
     }
 
-    /**
-     * Ensure that the playerID is unique
-     *
-     * @param name the player name to set
-     */
-    public void setName(String name) {
-        this.name = name;
+    @Override
+    public String toString() {
+        return name + ": " + hand + " (Total: " + getHandValue() + ")";
     }
-
-    /**
-     * The method to be overridden when you subclass the Player class with your specific type of Player and filled in
-     * with logic to play your game.
-     */
-    public abstract void play();
-
 }
